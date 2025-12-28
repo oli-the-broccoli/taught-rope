559509b84c5c86950a8e16dbe79131f9e3da8956
oli-the-broccoli <oliver@hopegf.com>
2025-12-28 15:51:05 +0200

    added a test scene

---
+++ test_scene.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export var rope: TaughtRope
+@export var rope_speed: float = 100
+
+
+func _process(delta: float) -> void:
+	var direction: Vector2 = Vector2.ZERO
+	if Input.is_key_pressed(KEY_UP):
+		direction.y -= 1
+	if Input.is_key_pressed(KEY_DOWN):
+		direction.y += 1
+	if Input.is_key_pressed(KEY_LEFT):
+		direction.x -= 1
+	if Input.is_key_pressed(KEY_RIGHT):
+		direction.x += 1
+	
+	rope.rope_end += direction.normalized() * rope_speed * delta
