d1887a6ac6989de2c148b44a333a2ef91c38b6bf
oli-the-broccoli <oliver@hopegf.com>
2026-01-15 16:02:11 +0200

    created soft rope test scene

---
+++ soft_rope_test.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var soft_rope: SoftRope
+
+func _process(delta: float) -> void:
+	soft_rope.start_point = $Camera2D.get_global_mouse_position()
+	var direction: Vector2 = Vector2.ZERO
+	if Input.is_key_pressed(KEY_UP):
+		direction.y -= 1
+	if Input.is_key_pressed(KEY_DOWN):
+		direction.y += 1
+	if Input.is_key_pressed(KEY_LEFT):
+		direction.x -= 1
+	if Input.is_key_pressed(KEY_RIGHT):
+		direction.x += 1
+	
+	#soft_rope.start_point += direction.normalized() * 100 * delta
