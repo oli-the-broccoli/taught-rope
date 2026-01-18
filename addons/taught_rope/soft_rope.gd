45defcbd1c8dc026cf4bdca90525c8d500fc8598
oli-the-broccoli <oliver@hopegf.com>
2026-01-18 19:09:11 +0200

    restructured variables and switched from num segments to segment length

--- addons/taught_rope/soft_rope.gd
+++ addons/taught_rope/soft_rope.gd
@@ -1,21 +1,33 @@
 class_name SoftRope
 extends Node2D
 
+@export var rope_length: float = 100:
+	set(value):
+		rope_length = value
+		calc_segments()
 
-@export var rope_length: float = 100
-@export var segments: int = 10
+@export var target_segment_length: float = 10:
+	set(value):
+		target_segment_length = value
+		calc_segments()
+
+@export var rope_width: float = 2:
+	set(value):
+		rope_width = value
+		if collision_radius == 0:
+			collision_circle.radius = rope_width/2
+
 @export var itterations: int = 10:
 	set(value):
 		itterations = value
 		_normalised_bending_stiffness = bending_stiffness / value
 
 @export_group('Physics')
-@export_flags_2d_physics var collision_mask: int = 1
 @export_range(-10,10,0.5,'or_greater',"or_less") var gravity_scale: float = 1.0
 
 @export_subgroup('Friction')
 @export var damping: float = 1
-@export_range(0,1,0.01) var k_fp: float = 0.5
+@export_range(0, 1, 0.01) var k_fp: float = 0.5
 @export var k_fc: float = 0.5
 
 @export_subgroup('Bending')
@@ -26,12 +38,23 @@ extends Node2D
 @export var free_bending_radius: float = 20
 
 @export_subgroup('Collisions')
+@export_flags_2d_physics var collision_mask: int = 1:
+	set(value):
+		collision_mask = value
+		node_query.collision_mask = value
 @export var collision_itterations: int = 1
 @export var resolve_collisions_while_constraining: bool = false
+@export var collision_radius: float = 0:
+	set(value):
+		collision_radius = value
+		if value == 0:
+			collision_circle.radius = rope_width/2
+		else:
+			collision_circle.radius = collision_radius
+		node_query.shape = collision_circle
 
 @export_group('Rendering')
 @export var draw_rope: bool = true
-@export var rope_width: float = 2
 @export var rope_color: Color = Color.SADDLE_BROWN
 @export var draw_nodes: bool = false
 @export var node_radius: float = 1
@@ -39,7 +62,9 @@ extends Node2D
 
 var _gravity: Vector2
 
+var _segments: int
 var _segment_length: float 
+var num_nodes: int
 
 var _normalised_bending_stiffness: float = bending_stiffness / itterations
 
@@ -51,31 +76,41 @@ var velocities: Array[Vector2]
 var collision_normals: Array[Vector2]
 
 var space: PhysicsDirectSpaceState2D
+var collision_circle: CircleShape2D = CircleShape2D.new()
 var node_query: PhysicsShapeQueryParameters2D = PhysicsShapeQueryParameters2D.new()
+
+func calc_segments()->void:
+	_segments = round(rope_length / target_segment_length)
+	_segment_length = rope_length / _segments
+	num_nodes = _segments + 1
 
 func _ready() -> void:
 	space = get_world_2d().direct_space_state
-	positions.resize(segments+1)
-	_prev_positions.resize(segments+1)
-	velocities.resize(segments+1)
+	
+	positions.resize(num_nodes)
+	_prev_positions.resize(num_nodes)
+	velocities.resize(num_nodes)
 	velocities.fill(Vector2.ZERO)
-	collision_normals.resize(segments + 1)
+	collision_normals.resize(num_nodes)
 	collision_normals.fill(Vector2.ZERO)
+	
 	_gravity = gravity_scale * ProjectSettings.get_setting('physics/2d/default_gravity_vector') * ProjectSettings.get_setting('physics/2d/default_gravity')
-	_segment_length = rope_length/segments
-	for i:int in range(positions.size()):
+	_segment_length = rope_length/_segments
+	for i:int in range(num_nodes):
 		positions[i] = Vector2(0,i * _segment_length)
 	_prev_positions = positions.duplicate(true)
 	
-	node_query.collision_mask = collision_mask
-	var circle = CircleShape2D.new()
-	circle.radius = rope_width/2
-	node_query.shape = circle
+	if collision_radius == 0:
+		collision_circle.radius = rope_width/2
+	else:
+		collision_circle.radius = collision_radius
+	node_query.shape = collision_circle
+	
 	
 
 func _physics_process(delta: float) -> void:
 	
-	simulate(delta)
+	_simulate(delta)
 	var col_period: int
 	if resolve_collisions_while_constraining:
 		col_period = 1
@@ -85,25 +120,25 @@ func _physics_process(delta: float) -> void:
 	for i:int in range(itterations):
 		#points being keyframed should be set here
 		
-		constrain()
-		resolve_bending()
+		_constrain()
+		_resolve_bending()
 		if (i + 1) % col_period == 0:
-			resolve_collisions()
+			_resolve_collisions()
 		
 		positions[0] = start_point
 	
-	calc_velocities()
+	_calc_velocities()
 	collision_normals.fill(Vector2.ZERO)
 	
 	queue_redraw()
 
-func simulate(delta:float)->void:
+func _simulate(delta:float)->void:
 	for i:int in range(positions.size()):
 		velocities[i] += _gravity * delta
 		velocities[i] *= max(1-damping*delta,0)
 		positions[i] = positions[i] + velocities[i] * delta
 
-func constrain() ->void:
+func _constrain() ->void:
 	#length constraint
 	for i:int in range(1,positions.size()):
 		var segment: Vector2 = (positions[i] - positions[i-1])
@@ -116,7 +151,7 @@ func constrain() ->void:
 			positions[i-1] += move
 	
 
-func resolve_bending()->void:
+func _resolve_bending()->void:
 	if not bending_stiffness == 0:
 		# ef describes the distance from the i to the midpoint between i-1 and i+1 when they are respectively equistance from i
 		# in the condition that the segments lie on a circle of radius free_bending_radius
@@ -132,14 +167,12 @@ func resolve_bending()->void:
 				var e1: float = abs(segment.dot(norm))
 				var e2: float = abs(span.dot(norm))
 				var e: float = e2/2 + e1
-				#if i == segments - 1:
-					#print(e2/2, ' ', e1, ' ', e, ' ', ef)
 				var h: float = (1.0/3.0) * e * _normalised_bending_stiffness
 				positions[i-1] += norm * h
 				positions[i] += - norm * 2 * h
 				positions[i+1] += norm * h
 
-func resolve_collisions()->void:
+func _resolve_collisions()->void:
 	for i:int in range(positions.size()):
 		#having no motion currently means the impact absorbs all energy
 		#I can't really work out how this paramater is supposed to be used correctly
@@ -147,24 +180,29 @@ func resolve_collisions()->void:
 		node_query.transform = Transform2D(0,positions[i])
 		var result: Dictionary = space.get_rest_info(node_query)
 		if result != {}:
-			positions[i] = result.point + result.normal * (rope_width / 2 + 0.01)
+			positions[i] = result.point + result.normal * (collision_circle.radius + 0.01)
 			collision_normals[i] = result.normal
 
-func calc_velocities()->void:
+func _calc_velocities()->void:
 	for i:int in range(positions.size()):
 		velocities[i] = (positions[i] - _prev_positions[i]) / self.get_physics_process_delta_time()
+		
+		#apply friction
 		if not collision_normals[i].is_zero_approx():
 			var v_norm: Vector2 = velocities[i].dot(collision_normals[i]) * collision_normals[i]
 			var v_tan: Vector2 = velocities[i] - v_norm
 			v_tan *= (1-k_fp)
 			v_tan -= min(k_fc,v_tan.length()) * v_tan.normalized()
 			velocities[i] = v_tan + v_norm
+		
 		_prev_positions[i] = positions[i]
 
 func _draw() -> void:
 	draw_set_transform_matrix(self.transform.inverse())
+	
 	if draw_rope:
 		draw_polyline(positions,rope_color,rope_width,true)
+	
 	if draw_nodes:
 		for point: Vector2 in positions:
 			draw_circle(point,node_radius,node_color)
