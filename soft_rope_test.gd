63ce7fb84246104360f9febf054427856761aece
oli-the-broccoli <oliver@hopegf.com>
2026-01-19 18:52:32 +0200

    tested pins

--- soft_rope_test.gd
+++ soft_rope_test.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var soft_rope: SoftRope
 
 func _process(delta: float) -> void:
-	soft_rope.start_point = $Camera2D.get_global_mouse_position()
+	$RopeHandle.position = $Camera2D.get_global_mouse_position()
 	var direction: Vector2 = Vector2.ZERO
 	if Input.is_key_pressed(KEY_UP):
 		direction.y -= 1
